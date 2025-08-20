ce9a558a8c764b4ded013accdaae256d26e8817c
Aleksei Korkoza <alexeykorkoza@gmail.com>
2025-08-20 12:01:47 +0300

    feature: add styles to create url form

--- frontend/src/pages/create-url/ui/CreateShortUrlForm/CreateShortUrlForm.tsx
+++ frontend/src/pages/create-url/ui/CreateShortUrlForm/CreateShortUrlForm.tsx
@@ -11,6 +11,7 @@ import { createShortUrlSchema } from "@/entities/url/validations/create-short-ur
 import { ROUTE_PATHS } from "@/shared/routes";
 import { Button } from "@/shared/ui/Button";
 import { Calendar } from "@/shared/ui/Calendar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/shared/ui/Card";
 import { Form } from "@/shared/ui/Form";
 import { FormControl } from "@/shared/ui/FormControl";
 import { FormDescription } from "@/shared/ui/FormDescription";
@@ -69,107 +70,114 @@ export const CreateShortUrlForm = () => {
 	};
 
 	return (
-		<div>
-			<Form {...formMethods}>
-				<form onSubmit={handleSubmit(submitForm)}>
-					<FormField
-						control={control}
-						name="originalUrl"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Original Url</FormLabel>
-								<FormControl>
-									<Input {...field} placeholder="http://original_url.com" />
-								</FormControl>
-								<FormDescription />
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-
-					<FormField
-						control={control}
-						name="alias"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Alias</FormLabel>
-								<FormControl>
-									<Input {...field} />
-								</FormControl>
-								<FormDescription />
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-
-					<FormField
-						control={control}
-						name="expiredAt"
-						render={({ field: { onChange, value: date } }) => (
-							<FormItem>
-								<FormLabel>Expire At</FormLabel>
-								<Popover open={open} onOpenChange={setOpen}>
-									<PopoverTrigger asChild>
-										<Button
-											variant="outline"
-											id="date"
-											className="w-48 justify-between font-normal px-2"
-											style={date ? { paddingInline: "0" } : {}}
-										>
-											{date ? (
-												<div className="flex items-center justify-between w-full">
-													<div className="flex items-center gap-1">
-														<span>{date.toLocaleDateString()}</span>
-														<Icons.chevronDownIcon />
-													</div>
-
-													<Slot
-														onClick={(e) => {
-															clearExpireAtField(e);
-														}}
-														style={{ display: "inline-flex" }}
-													>
-														<Icons.close />
-													</Slot>
-												</div>
-											) : (
-												<div className="flex items-center gap-1 w-full">
-													<span>Select date</span>
-													<Icons.chevronDownIcon />
-												</div>
-											)}
-										</Button>
-									</PopoverTrigger>
-									<PopoverContent
-										className="w-auto overflow-hidden p-0"
-										align="start"
-									>
+		<div className="p-4">
+			<Card>
+				<CardHeader>
+					<CardTitle>Create Short Url</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<Form {...formMethods}>
+						<form onSubmit={handleSubmit(submitForm)}>
+							<FormField
+								control={control}
+								name="originalUrl"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Original Url</FormLabel>
 										<FormControl>
-											<Calendar
-												mode="single"
-												selected={date}
-												captionLayout="dropdown"
-												onSelect={(date) => {
-													onChange(date);
-													setOpen(false);
-												}}
-											/>
+											<Input {...field} placeholder="http://original_url.com" />
 										</FormControl>
-									</PopoverContent>
-								</Popover>
-								<FormDescription />
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<div className="flex items-center justify-center gap-2 my-2">
-						<Button asChild variant="outline">
-							<Link to={ROUTE_PATHS.urls}>Back</Link>
-						</Button>
-						<Button type="submit">Create</Button>
-					</div>
-				</form>
-			</Form>
+										<FormDescription />
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+
+							<FormField
+								control={control}
+								name="alias"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Alias</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormDescription />
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+
+							<FormField
+								control={control}
+								name="expiredAt"
+								render={({ field: { onChange, value: date } }) => (
+									<FormItem>
+										<FormLabel>Expire At</FormLabel>
+										<Popover open={open} onOpenChange={setOpen}>
+											<PopoverTrigger asChild>
+												<Button
+													variant="outline"
+													id="date"
+													className="w-48 justify-between font-normal px-2"
+													style={date ? { paddingInline: "0" } : {}}
+												>
+													{date ? (
+														<div className="flex items-center justify-between w-full">
+															<div className="flex items-center gap-1">
+																<span>{date.toLocaleDateString()}</span>
+																<Icons.chevronDownIcon />
+															</div>
+
+															<Slot
+																onClick={(e) => {
+																	clearExpireAtField(e);
+																}}
+																style={{ display: "inline-flex" }}
+															>
+																<Icons.close />
+															</Slot>
+														</div>
+													) : (
+														<div className="flex items-center gap-1 w-full">
+															<span>Select date</span>
+															<Icons.chevronDownIcon />
+														</div>
+													)}
+												</Button>
+											</PopoverTrigger>
+											<PopoverContent
+												className="w-auto overflow-hidden p-0"
+												align="start"
+											>
+												<FormControl>
+													<Calendar
+														mode="single"
+														selected={date}
+														captionLayout="dropdown"
+														onSelect={(date) => {
+															onChange(date);
+															setOpen(false);
+														}}
+													/>
+												</FormControl>
+											</PopoverContent>
+										</Popover>
+										<FormDescription />
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<div className="flex items-center justify-center gap-2 my-2">
+								<Button asChild variant="outline">
+									<Link to={ROUTE_PATHS.urls}>Back</Link>
+								</Button>
+								<Button type="submit">Create</Button>
+							</div>
+						</form>
+					</Form>
+				</CardContent>
+			</Card>
 		</div>
 	);
 };
