c66520e640c1ca26a8f72af086045b63cc3c7499
Aleksei Korkoza <alexeykorkoza@gmail.com>
2025-09-01 14:55:00 +0300

    feature: add biome config to package, apply it to all apps

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,30 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { ConfigService } from "@nestjs/config";
+import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
-import { AppModule } from '@/modules/app/app.module';
+import { AppModule } from "@/modules/app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  const port = configService.get('port') as string;
-  const globalPrefix = configService.get('apiPrefix') as string;
+  const port = configService.get("port") as string;
+  const globalPrefix = configService.get("apiPrefix") as string;
   app.setGlobalPrefix(globalPrefix, {
-    exclude: [':shortUrl'],
+    exclude: [":shortUrl"],
   });
-  app.enableCors({
-    origin: ['http://localhost:5173'],
-  });
+  app.enableCors({ origin: ["http://localhost:5173"] });
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('Short URL API')
-    .setVersion('1.0')
+    .setTitle("Short URL API")
+    .setVersion("1.0")
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup("api", app, documentFactory);
 
   await app.listen(port);
 }
